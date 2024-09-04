0b2b20394a5f0e1899e9459c305ee8cbbcc308ff
Admin <nikita.ishmekeev.2003@gmail.com>
2024-09-04 13:13:19 +0500

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите m");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите n");
+        int k = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Sum(m)+ Sum(k));
+    }
+
+    public static int Sum(int n)
+    {
+        if (n == 1) return 1;
+        else return n + Sum(n-1);
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void PrintDigits(int n)
+    {
+        if (n < 10)
+        {
+            Console.Write(n);
+        }
+        else
+        {
+            PrintDigits(n / 10);
+            Console.Write(n % 10);
+        }
+    }
+
+    static void ProcessRange(int A, int B)
+    {
+        for (int i = A; i <= B; i++)
+        {
+            PrintDigits(i);
+            Console.WriteLine(); 
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите A: ");
+        int A = int.Parse(Console.ReadLine());
+        Console.Write("Введите B: ");
+        int B = int.Parse(Console.ReadLine());
+        ProcessRange(A, B);
+    }
+}
